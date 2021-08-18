5f2c8cd9b373fcc304aa685bb46f5240b317c4ef
SajjdManafi <sajadmanafi8@gmail.com>
2021-08-18 23:08:57 +0430

    Short variable Declaration

---
+++ 01-Variables/00_Short_Declaration.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// declare variable and assign a value
+	x := 20
+	// assigning
+	x = 21
+	fmt.Println(x)
+
+	y := 990 + 9
+	fmt.Println(y)
+
+	z := "Hey...!"
+	fmt.Println(z)
+
+	// you cant have unused variables in your go code. the compiler doesen't allow it.
+}
