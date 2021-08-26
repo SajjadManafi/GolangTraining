7f309b4d6962749b5cedb3f6464f45ba6a05d017
SajjdManafi <sajadmanafi8@gmail.com>
2021-08-26 17:14:20 +0430

    bit shifting

---
+++ 02-Types/05_Bit_Shifting.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// example for iota and bit shifting
+const (
+	_  = iota
+	kb = 1 << (iota * 10)
+	mb = 1 << (iota * 10)
+	gb = 1 << (iota * 10)
+)
+
+func main() {
+
+	// "<<" and ">>" using for bit shifting
+
+	x := 4
+	fmt.Printf("%d\t\t%b\n", x, x)
+
+	y := x << 1
+	fmt.Printf("%d\t\t%b\n\n", y, y)
+
+	fmt.Printf("%d\t\t\t%b\n", kb, kb)
+	fmt.Printf("%d\t\t\t%b\n", mb, mb)
+	fmt.Printf("%d\t\t%b\n", gb, gb)
+
+}
