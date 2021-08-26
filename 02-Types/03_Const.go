677c3370612819ef1966b939ec26849f2bf0afe9
SajjdManafi <sajadmanafi8@gmail.com>
2021-08-26 17:13:38 +0430

    Const

---
+++ 02-Types/03_Const.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+const hey = "hey!"
+const hello string = "Hello..."
+
+const x = 13
+
+const (
+	y = 45.34
+	a = "z"
+)
+
+func main() {
+
+	//consts:
+	//a simple, unchanging value
+	//Only exist at compile time.
+
+	fmt.Println(hey)
+	fmt.Printf("%T\n", hey)
+
+	fmt.Println(hello)
+	fmt.Printf("%T\n", hello)
+
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+
+	fmt.Println(y, a)
+
+}
