d0696a62be9f18875ff9a537150b3580f54dfb00
SajjdManafi <sajadmanafi8@gmail.com>
2021-08-31 18:15:19 +0430

    Fan out pattarn

---
+++ 09-Channels/07_Fan_Out.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+// Taking some work and putting the chunks of work onto many goroutines.
+
+func main() {
+	c1 := make(chan int)
+	c2 := make(chan int)
+
+	go populate(c1)
+
+	go fanOutIn(c1, c2)
+
+	for v := range c2 {
+		fmt.Println(v)
+	}
+
+	fmt.Println("about to exit")
+}
+
+func populate(c chan int) {
+	for i := 0; i < 100; i++ {
+		c <- i
+	}
+	close(c)
+}
+
+func fanOutIn(c1, c2 chan int) {
+	var wg sync.WaitGroup
+	for v := range c1 {
+		wg.Add(1)
+		go func(v2 int) {
+			c2 <- timeConsumingWork(v2)
+			wg.Done()
+		}(v)
+	}
+	wg.Wait()
+	close(c2)
+}
+
+func timeConsumingWork(n int) int {
+	time.Sleep(time.Microsecond * time.Duration(rand.Intn(500)))
+	return n + rand.Intn(1000)
+}
+
+// Todd_McLeod
+// source : https://play.golang.org/p/iU7Oee2nm7
